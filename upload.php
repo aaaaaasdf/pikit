e445dca0ac25061399ce933cf89c4cbceb2204a7
aaaaaasdf <clashboyjl@gmail.com>
2021-05-12 17:58:56 +0100

    add upload.php and style/script

---
+++ upload.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link rel="stylesheet" href="upload_style.css">
+    <div><br>
+        <center><h3>PikIt Free Photo Hosting Service</h3></center><br>
+    </div>
+</head>
+
+<body>
+    <script src="upload_script.js"> </script>
+
+</body>
+
+</html>
