5d989f94a29836d4b00d5a63f0ff5aae9ae43e16
aaaaaasdf <clashboyjl@gmail.com>
2021-05-12 17:34:53 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link rel="stylesheet" href="index_style.css">
+    <div><br>
+        <center><h3>PikIt Free Photo Hosting Service</h3></center><br>
+    </div>
+</head>
+
+<body>
+    <script src="index_script.js"> </script>
+</body>
+
+</html>
